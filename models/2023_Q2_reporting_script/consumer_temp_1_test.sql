e25708e433831e5c2fe85ef81e7df3edb9d9981e
dhruv.b@data-nectar.com <dhruv.b@data-nectar.com>
2023-11-08 11:45:55 +0000

    update consume_temp_1_test.sql

--- models/2023_Q2_reporting_script/consumer_temp_1_test.sql
+++ models/2023_Q2_reporting_script/consumer_temp_1_test.sql
@@ -85,55 +85,5 @@ select
         else 'Unknown'
     end as aid_1
 from consumer_temp_1_update
-),
-drop_row_cte as (
-select
-uuid,
-question_id,
-ranking,
-MAX(ranking) OVER (PARTITION BY uuid) maximums
-from(
-    select q1.uuid, 
-    question_id,
-    substr(question_id,7,1) ranking
-    from tvc_surveys_prod.forsta_dev_prod.responses q1 
-    inner join(
-        (select 
-        uuid,
-        count(*) total
-        from(
-            select q1.* 
-            from tvc_surveys_prod.forsta_dev_prod.responses q1
-            inner join (
-               select uuid
-                from tvc_surveys_prod.forsta_dev_prod.responses
-                where question_id = 'status' and answer_id = '3' ) q2 on q1.uuid = q2.uuid
-            where contains(question_id, 'demo4') and answer_id = 1 )
-        group by uuid
-        having total > 1)) q2 on  q1.uuid = q2.uuid
-    where contains(question_id, 'demo4') and answer_id = 1
-    )
-    order by uuid
-),
-consumer_temp_1_update_3 as (
-select
-    ctu.uuid
-    ,ctu.qid_1
-    ,ctu.aid_1
-    ,case
-        when substr(ctu.qid_1, 1, 5) = 'demo4' and ctu.aid_1 = '0' then 1
-        when contains(ctu.qid_1, 'oe') then 1
-        when contains(ctu.qid_1, 'spec') then 1
-        when ctu.qid_1 = 'demo2' then 1
-        when substr(ctu.qid_1,1,2) = 'vc' and ctu.aid_1 != '1' then 1
-        when ctu.qid_1 = 'demo1' then 1
-        when ctu.uuid in (select uuid from consumer_temp_1 where qid_1 = 'demo1' and aid_1 !='1') then 1
-        when ctu.qid_1 = 'demo3' and ctu.aid_1 > '2' then 1
-        when ctu.aid_1 = 'Unknown' then 1
-        when drc.ranking != drc.maximums then 1
-    end as drop_row
-from consumer_temp_1_update_2 ctu
-left join drop_row_cte drc on drc.uuid = ctu.uuid and drc.question_id = ctu.qid_1
--- where drc.ranking != drc.maximums
 )
-select * from consumer_temp_1_update_3
+select * from consumer_temp_1_update_2
